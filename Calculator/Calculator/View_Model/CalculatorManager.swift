646f0f0e922c883f18a5f380aacc93174159974a
Bassirou <sarr.thierry@outlook.fr>
2024-10-25 14:12:57 +0200

    second commit

---
+++ Calculator/Calculator/View_Model/CalculatorManager.swift
@@ -0,0 +1,11 @@
+//
+//  CalculatorManager.swift
+//  Calculator
+//
+//  Created by Thierry Sarr on 25/10/2024.
+//
+
+import Foundation
+class CalculatorManager: ObservableObject{
+    
+}
---
+++ Calculator/Calculator/supporting files/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Thierry Sarr on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
